90665eb4b6369c8591fb105d6a92fc56f1aea54a
bp10113490 <bianca.petrut00@e-uvt.ro>
2022-10-19 16:57:58 +0300

    testing

--- src/ro/uvt/p3/io/InputDevice.java
+++ src/ro/uvt/p3/io/InputDevice.java
@@ -3,6 +3,6 @@ package ro.uvt.p3.io;
 public class InputDevice {
 
     public String nextLine(){
-        return "The quick brown fox jumps over the lazy dog.";
+        return "The quick fox jumps over the lazy dog.";
     }
 }
